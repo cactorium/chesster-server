12959b492b30bf37c732b2859840839cf6db9887
Kelvin Ly <kelvin.ly1618@gmail.com>
2018-07-11 23:33:23 -0400

    Test castling

--- chesster/board_test.go
+++ chesster/board_test.go
@@ -36,9 +36,6 @@ func TestValidTwoKingsPinnedBishop(t *testing.T) {
 	b.Pieces = append(b.Pieces, Piece{4, 3, Rook, White, true})
 	b.Pieces = append(b.Pieces, Piece{4, 5, Bishop, Black, true})
 
-	for _, p := range b.Pieces {
-		fmt.Printf("{%d %d %v %v %v}\n", p.X, p.Y, p.Type, p.Side, p.HasMoved)
-	}
 	ms := b.Pieces[0].GetPossibleMoves(&b)
 	if len(ms) != 5 {
 		for _, m := range ms {
@@ -66,6 +63,53 @@ func TestValidTwoKingsPinnedBishop(t *testing.T) {
 	if len(ms) != 0 {
 		t.Errorf("expected %d got %d", 0, len(ms))
 	}
+}
+
+func TestValidCastle(t *testing.T) {
+	b := Board{}
+	b.Pieces = append(b.Pieces, Piece{4, 0, King, White, false})
+	b.Pieces = append(b.Pieces, Piece{4, 6, King, Black, true})
+	b.Pieces = append(b.Pieces, Piece{7, 0, Rook, White, false})
+	ms := b.Pieces[0].GetPossibleMoves(&b)
+	if len(ms) != 6 {
+		t.Errorf("expected %d got %d", 6, len(ms))
+	}
+	ms = b.Pieces[1].GetPossibleMoves(&b)
+	if len(ms) != 8 {
+		t.Errorf("expected %d got %d", 8, len(ms))
+	}
+	ms = b.Pieces[2].GetPossibleMoves(&b)
+	if len(ms) != 9 {
+		t.Errorf("expected %d got %d", 9, len(ms))
+	}
+
+	b.Pieces = append(b.Pieces, Piece{5, 7, Rook, Black, true})
+	ms = b.Pieces[0].GetPossibleMoves(&b)
+	if len(ms) != 3 {
+		t.Errorf("expected %d got %d", 3, len(ms))
+	}
+	ms = b.Pieces[1].GetPossibleMoves(&b)
+	if len(ms) != 7 {
+		for _, m := range ms {
+			fmt.Printf("%s\n", m.Notation(&b))
+		}
+		t.Errorf("expected %d got %d", 7, len(ms))
+	}
+	ms = b.Pieces[2].GetPossibleMoves(&b)
+	if len(ms) != 9 {
+		for _, m := range ms {
+			fmt.Printf("%s\n", m.Notation(&b))
+		}
+		t.Errorf("expected %d got %d", 9, len(ms))
+	}
+	ms = b.Pieces[3].GetPossibleMoves(&b)
+	if len(ms) != 14 {
+		for _, m := range ms {
+			fmt.Printf("%s\n", m.Notation(&b))
+		}
+		t.Errorf("expected %d got %d", 14, len(ms))
+	}
+
 }
 
 // TODO check pawns
